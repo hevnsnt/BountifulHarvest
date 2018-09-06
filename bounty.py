456b97f035eb2b87c3f189a050c76bc123cb839d
Bill Swearingen <hevnsnt@gmail.com>
2018-09-05 20:04:30 -0500

    email generation function complete

--- bounty.py
+++ bounty.py
@@ -4,25 +4,51 @@ import random
 import string
 import json
 
-chars = string.ascii_letters + string.digits + '!@#$%^&*()'
-random.seed = (os.urandom(1024))
-
 url = 'http://craigslist.pottsfam.com/index872dijasydu2iuad27aysdu2yytaus6d2ajsdhasdasd2.php'
 
-domains = json.loads(open('domains.json').read())
-lnames = json.loads(open('lnames.json').read())
-female = json.loads(open('female-fname.json').read())
-male = json.loads(open('male-fname.json').read())
+##### INIT SECTION #####
+chars = string.ascii_letters + string.digits + '!@#$%^&*()'
+#creates char set 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
+random.seed = (os.urandom(1024))
 
-for name in names:
-	name_extra = ''.join(random.choice(string.digits))
 
-	username = name.lower() + name_extra + '@yahoo.com'
-	password = ''.join(random.choice(chars) for i in range(8))
 
-	requests.post(url, allow_redirects=False, data={
-		'auid2yjauysd2uasdasdasd': username,
-		'kjauysd6sAJSDhyui2yasd': password
-	})
+def loadArray():
+	domains = json.loads(open('domains.json').read())
+	lnames = json.loads(open('lnames.json').read())
+	female = json.loads(open('female-fname.json').read())
+	male = json.loads(open('male-fname.json').read())
+	return(domains, lnames, female, male)
 
-print 'sending username %s and password %s' % (username, password)
+def getName():
+	personType = random.choice(['male','female'])
+	email_format = random.choice([1, 2, 3])
+	lname = random.choice(lnames)
+	if personType is 'male':
+		fname = random.choice(male)
+	else:
+		fname = random.choice(female)
+	if email_format is 1:
+		print(fname + "." + lname + "@" + random.choice(domains))
+	elif email_format is 2:
+		print(fname + "_" + lname + "@" + random.choice(domains))
+	else:
+		print(fname[0] + lname + "@" + random.choice(domains))
+
+
+def post(count):
+	for name in names:
+		name_extra = ''.join(random.choice(string.digits))
+
+		username = name.lower() + name_extra + '@yahoo.com'
+		password = ''.join(random.choice(chars) for i in range(8))
+
+		requests.post(url, allow_redirects=False, data={
+			'auid2yjauysd2uasdasdasd': username,
+			'kjauysd6sAJSDhyui2yasd': password
+		})
+
+
+if __name__ == "__main__": # execute only if run as a script
+	domains, lnames, female, male = loadArray()
+	print 'sending username %s and password %s' % (username, password)
